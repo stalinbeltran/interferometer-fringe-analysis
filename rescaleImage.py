e68049262acfe5dd89d95ee9bcce8c9fcbb86277
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-25 15:49:22 -0500

    save rescaled

--- rescaleImage.py
+++ rescaleImage.py
@@ -1,16 +1,21 @@
+#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python equalizeImage.py ./images/alineacion666.png
+
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from skimage import exposure
 import sys
+import os
 
 archivoProcesar = sys.argv[1]
 print('file:',archivoProcesar)
 img = cv2.imread(archivoProcesar, cv2.IMREAD_GRAYSCALE)
-print(img)
+print('img: ', img)
 
 img2 = exposure.rescale_intensity(img)
-print(img2)
+print('img2: ', img2)
+filename, file_extension = os.path.splitext(archivoProcesar)
+cv2.imwrite(filename + "_rescaled" + file_extension, img2)
 cv2.imshow("Image", img2)
 
 cv2.waitKey(0)
