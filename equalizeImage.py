d0c7bc99d44228fccbe63361b49c983bface285a
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-25 15:10:48 -0500

    rescale intensity (I can't see the difference)

--- equalizeImage.py
+++ equalizeImage.py
@@ -1,19 +1,17 @@
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from skimage import exposure
 import sys
 
 archivoProcesar = sys.argv[1]
 print('file:',archivoProcesar)
 img = cv2.imread(archivoProcesar, cv2.IMREAD_GRAYSCALE)
+print(img)
 
-fig = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(1, 2)
-ax1 = fig.add_subplot(gs[0, 0])
-ax1.imshow(img, cmap='gray')
-plt.show()
-
-cv2.imshow("Image", img)
+img2 = exposure.rescale_intensity(img)
+print(img2)
+cv2.imshow("Image", img2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
