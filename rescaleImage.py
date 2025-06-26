218d3680b818b0230158ae34a6a42b3e159e304a
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-26 10:47:40 -0500

    rescale a folder

--- rescaleImage.py
+++ rescaleImage.py
@@ -1,4 +1,4 @@
-#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python rescaleImage.py ./images/alineacion666.png
+#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python rescaleImage.py ./originales ./recortadas
 
 import cv2
 import matplotlib.pyplot as plt
@@ -7,18 +7,18 @@ from skimage import exposure
 import sys
 import os
 
-archivoProcesar = sys.argv[1]
-print('file:',archivoProcesar)
-img = cv2.imread(archivoProcesar, cv2.IMREAD_GRAYSCALE)
-print('img: ', img)
+input_folder = (sys.argv[1])
+output_folder = (sys.argv[2])
+os.makedirs(output_folder, exist_ok=True)
 
-img2 = exposure.rescale_intensity(img)
-print('img2: ', img2)
-filename, file_extension = os.path.splitext(archivoProcesar)
-cv2.imwrite(filename + "_rescaled" + file_extension, img2)
-cv2.imshow("Image", img2)
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        inputPath = os.path.join(input_folder, filename)
+        filenameNoExt, file_extension = os.path.splitext(filename)
+        outputPath = os.path.join(output_folder, filenameNoExt + "_rescaled" + file_extension)
+        img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
+        img2 = exposure.rescale_intensity(img)
+        cv2.imwrite(outputPath, img2)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
