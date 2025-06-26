dae407caa8435b775a8fe7da30c8d77f7c59e4a0
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-26 15:54:42 -0500

    .

--- equalizeImage.py
+++ equalizeImage.py
@@ -17,23 +17,21 @@ guessedWavelength = float(sys.argv[2])
 print('file:',archivoProcesar)
 
 fig = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(3, 2)
-ax1 = fig.add_subplot(gs[0, 1])
-ax2 = fig.add_subplot(gs[0, 0])
-ax3 = fig.add_subplot(gs[1, :])
-ax4 = fig.add_subplot(gs[2, :])
+gs = gridspec.GridSpec(1, 2)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[0, 1])
 ax1.set_xticks([])
 ax1.set_yticks([])
 ax2.set_yticks([])
 ax2.set_xticks([])
 
 img = cv2.imread(archivoProcesar, 0) # read in the image as grayscale
-imgnew = img
+
 yp, xp = np.where(img != 0)
 xmax = max(xp)
 xmin = min(xp)
-ax2.imshow(img, cmap='gray') #show the derivative (troughs are very visible)
-ax2.set_title("original: " + archivoProcesar)
+ax1.imshow(img, cmap='gray') #show the derivative (troughs are very visible)
+ax1.set_title("original: " + archivoProcesar)
 
 print()
 
@@ -42,24 +40,9 @@ outputFile = os.path.join(filenameNoExt + "_equalized" + file_extension)
 
 imageSineFit = isf.imageSineFit(archivoProcesar, outputFile, guessedWavelength)
 #print(imageSineFit)
-sys.exit()
+#sys.exit()
 
-print('phases size:', np.size(phases))
-print('mean phase:', meanPhase)
-print('std phase:', stdPhase)
-
-meanPhase, stdPhase, phases, paramListRight = scanImage(xmiddle, xmax, ax4)
-print('phases size:', np.size(phases))
-print('mean phase:', meanPhase)
-print('std phase:', stdPhase)
-#print('paramList:', paramList)
-ax3.set_title("Left Image")
-ax4.set_title("Right Image")
-
-filenameNoExt, file_extension = os.path.splitext(archivoProcesar)
-outputPath = os.path.join(filenameNoExt + "_equalized" + file_extension)
-img2 = imgnew
-cv2.imwrite(outputPath, img2)
-ax1.imshow(imgnew, cmap='gray')
-ax1.set_title("New image")
+imgnew = cv2.imread(imageSineFit["imagepath"]["output"], 0)
+ax2.imshow(imgnew, cmap='gray')
+ax2.set_title("New image")
 plt.show()
