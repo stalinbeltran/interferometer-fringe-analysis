59fef2507fad9b49b20c02fa75a3420fac463e1b
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-26 16:04:18 -0500

    copy fitImage

---
+++ fitImage.py
@@ -0,0 +1,24 @@
+#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python fitImage.py ./originales ./recortadas
+
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from skimage import exposure
+import sys
+import os
+
+input_folder = (sys.argv[1])
+output_folder = (sys.argv[2])
+os.makedirs(output_folder, exist_ok=True)
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        inputPath = os.path.join(input_folder, filename)
+        filenameNoExt, file_extension = os.path.splitext(filename)
+        outputPath = os.path.join(output_folder, filenameNoExt + "_rescaled" + file_extension)
+        img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
+        img2 = exposure.rescale_intensity(img)
+        cv2.imwrite(outputPath, img2)
+
+
+
