8c2e7ba202ead6641435030a04ff45022c66eaa1
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-26 16:21:01 -0500

    .

--- fitImage.py
+++ fitImage.py
@@ -1,4 +1,4 @@
-#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python fitImage.py ./originales ./recortadas
+#D:\Stalin\Desarrollo\interferometer-fringe-analysis>python fitImage.py ./videos/fringes_7/framesRescaled ./videos/fringes_7/framesFitImage ./videos/fringes_7/framesFitImagejson 300
 
 import cv2
 import matplotlib.pyplot as plt
@@ -9,16 +9,24 @@ import os
 
 input_folder = (sys.argv[1])
 output_folder = (sys.argv[2])
+outputJSON_folder = (sys.argv[3])
+guessedWavelength = float(sys.argv[4])
 os.makedirs(output_folder, exist_ok=True)
+os.makedirs(outputJSON_folder, exist_ok=True)
 
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         inputPath = os.path.join(input_folder, filename)
+        outputPath = os.path.join(output_folder, filename)
         filenameNoExt, file_extension = os.path.splitext(filename)
-        outputPath = os.path.join(output_folder, filenameNoExt + "_rescaled" + file_extension)
-        img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
-        img2 = exposure.rescale_intensity(img)
-        cv2.imwrite(outputPath, img2)
+        filenameJSON = filenameNoExt + ".json"
+        outputPathJSON = os.path.join(outputJSON_folder, filenameJSON)
+        imageSineFit = isf.imageSineFit(inputPath, outputPath, guessedWavelength)
+        outputJSON_folder
+        with open(outputPathJSON, 'w') as f:
+            json.dump(imageSineFit, f)
+        print(outputPath, outputPathJSON)
+
 
 
 
