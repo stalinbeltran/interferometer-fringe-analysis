6800f03b472608dbedca084e47835ff3573b8c2b
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-27 17:12:08 -0500

    got average image

--- mean_images.py
+++ mean_images.py
@@ -21,14 +21,18 @@ for filename in os.listdir(input_folder):
     img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE) # read in the image as grayscale
     if imgf is None:
         imgf = img.astype(np.float32)
-    imgf += img
-    if c == 20: break
+    else:
+        imgf += img
+    #if c == 20: break
     c+=1
     
+imgf /= 1.0*c
 print(img[0:4, 0:2])
 print(imgf[0:4, 0:2])
-    # cv2.imshow('image', img)
-    # cv2.waitKey(0)
+imgMean = imgf.astype(np.uint8)
+print(np.max(imgMean))
+cv2.imshow('image', imgMean)
+cv2.waitKey(0)
 
 
 
