02083c2609b70985fbce8e176708136187823ecd
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-27 17:03:18 -0500

    add bunch of imgs

--- mean_images.py
+++ mean_images.py
@@ -12,15 +12,21 @@ import numpy as np
 
 input_folder = (sys.argv[1])
 
+imgf = None
+c = 0
 for filename in os.listdir(input_folder):
     if not filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         continue
     inputPath = os.path.join(input_folder, filename)
     img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE) # read in the image as grayscale
-    imgf = img.astype(np.float32)
-    print(img[0:4, 0:2])
-    print(imgf[0:4, 0:2])
-    break
+    if imgf is None:
+        imgf = img.astype(np.float32)
+    imgf += img
+    if c == 20: break
+    c+=1
+    
+print(img[0:4, 0:2])
+print(imgf[0:4, 0:2])
     # cv2.imshow('image', img)
     # cv2.waitKey(0)
 
