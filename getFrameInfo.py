6e5ba2e10bce6e09313f3b533af894869c57f57f
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-27 08:50:06 -0500

    json file

--- getFrameInfo.py
+++ getFrameInfo.py
@@ -13,7 +13,6 @@ import numpy as np
 
 def getMeanBySide(data, side):
     leftParameters = data["fitData"][side]["fitParameters"]
-    print(list(leftParameters[0].keys()))
     parameters = list(leftParameters[0].keys())
     parametersSize = len(parameters)
     leftParametersPhases = np.zeros(len(leftParameters))
@@ -25,7 +24,6 @@ def getMeanBySide(data, side):
         j +=1
         
     mean = np.mean(npParameters, axis = 1)
-    print('mean', mean)
     i = 0
     f = {}
     for param in parameters:
@@ -48,21 +46,13 @@ def getMean(data, filename):
         }
     }
 
-    print(frame)
     return frame
 
 
 
 
-
-
-
-
 input_folder = (sys.argv[1])
-output_folder = (sys.argv[2])
-#os.makedirs(output_folder, exist_ok=True)
-
-outputPath = os.path.join(output_folder, 'frames.json')
+output_file = (sys.argv[2])
 
 frameData = []
 for filename in os.listdir(input_folder):
@@ -72,39 +62,11 @@ for filename in os.listdir(input_folder):
         data = json.load(f)
     if not data:
         continue
-    # print('data', data)
-    # print(data.keys())
-    #dict_keys(['imagepath', 'fitData'])
-    fitData = data["fitData"]
-    #print(fitData.keys())
-    #dict_keys(['leftSide', 'rightSide'])
-    leftData = fitData["leftSide"]
-    #print(leftData.keys())
-    #dict_keys(['fitParameters'])
-    parameters = leftData["fitParameters"]
-    #print(parameters[0])
-    #{'amplitude': {'value': 52.445338132835005, 'error': 1.5758581058845498}, 'wavelength': {'value': 0.012312298713821406, 'error': 9.062006024708478e-05}, 'phase': {'value': 5.493020119917932, 'error': 0.055429869589786004}, 'verticalDisplacement': {'value': 105.12554035537349, 'error': 1.1043501120158479}}
-
     frame = getMean(data, filename)
     frameData.append(frame)
     break
     
 print('JSON', json.dumps(frameData))
-'''
-    imageSineFit = {
-        "imagepath": {
-            "input": inputFile, 
-            "output": outputFile
-        },
-        "fitData":{
-            "leftSide": {
-                "fitParameters": paramListLeft
-            },
-            "rightSide": {
-                "fitParameters": paramListRight
-            },
-        }
-
-    }
-	
-    '''
+with open(output_file, 'w') as f:
+    json.dump(frameData, f)
+#print(outputPath, outputPathJSON)
