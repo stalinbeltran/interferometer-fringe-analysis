78c0b19bc02553318358d790e6686f504b47e504
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-27 16:59:15 -0500

    image as float

--- mean_images.py
+++ mean_images.py
@@ -8,6 +8,7 @@ import sys
 import os
 import imageSineFit as isf
 import json
+import numpy as np
 
 input_folder = (sys.argv[1])
 
@@ -16,8 +17,12 @@ for filename in os.listdir(input_folder):
         continue
     inputPath = os.path.join(input_folder, filename)
     img = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE) # read in the image as grayscale
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
+    imgf = img.astype(np.float32)
+    print(img[0:4, 0:2])
+    print(imgf[0:4, 0:2])
+    break
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
 
 
 
