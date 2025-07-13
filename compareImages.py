3ab620aba8a0a9b77f706e013ddf6e61142c6875
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-13 13:23:59 -0500

    precision is not the problem

--- compareImages.py
+++ compareImages.py
@@ -12,8 +12,8 @@ img1 = cv2.imread("./videos/fringes_9/frames/fringes_9-369-.png", cv2.IMREAD_GRA
 # cv2.waitKey()
 
 
-img0_norm = img0/np.sqrt(np.sum(img0**2))
-img1_norm = img1/np.sqrt(np.sum(img1**2))
+img0_norm = 1.*img0/np.sqrt(np.sum(img0**2))
+img1_norm = 1.*img1/np.sqrt(np.sum(img1**2))
 
 print("np.sqrt(np.sum(img0**2): ", np.sqrt(np.sum(img0**2)))
 print("np.sqrt(np.sum(img1**2): ", np.sqrt(np.sum(img1**2)))
