391f5c1d8f55e5cb944645255d39a51f75c0b826
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-13 14:24:59 -0500

    check for all files

--- compareImages.py
+++ compareImages.py
@@ -1,24 +1,25 @@
 
 import numpy as np
 import cv2
-import sys
+import os
+import imageSineFit as isf
 
+input_folder = "./videos/fringes_9/frames/"   #(sys.argv[1])
 img0 = cv2.imread("./videos/fringes_9/frames/fringes_9-400-.png", cv2.IMREAD_GRAYSCALE)
-# cv2.imshow('', img0)
-# cv2.waitKey()
-
-img1 = cv2.imread("./videos/fringes_9/frames/fringes_9-369-.png", cv2.IMREAD_GRAYSCALE)
-# cv2.imshow('', img1)
-# cv2.waitKey()
-
-
 img0_norm = img0/np.sum(img0)
-img1_norm = img1/np.sum(img1)
 
-similaritiy = np.sum(np.sqrt(img0_norm*img0_norm))
-print("similaritiy (img0_norm*img0_norm): ", similaritiy)
-similaritiy = np.sum(np.sqrt(img0_norm*img1_norm))
-print("similaritiy (img0_norm*img1_norm): ", similaritiy)
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        inputPath = os.path.join(input_folder, filename)
+        filenameNoExt, file_extension = os.path.splitext(filename)
+        img1 = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
+        if isf.isBlackImage(img1):
+            continue                #ignore black images
+
+        img1_norm = img1/np.sum(img1)
+        similarity = np.sum(np.sqrt(img0_norm*img1_norm))
+        if similarity > 0.999:
+            print("archivo: ", filename, " - ", similarity)
 
 
 cv2.destroyAllWindows()
