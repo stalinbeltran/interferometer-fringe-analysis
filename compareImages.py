94b0e3506fea88200bf87b4abd612c17f561e5c0
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-13 12:41:11 -0500

    images showed

--- compareImages.py
+++ compareImages.py
@@ -1,16 +1,26 @@
 
+import numpy as np
 import cv2
 import sys
 
-img = cv2.imread("./videos/fringes_9/frames/fringes_9-400-.png", cv2.IMREAD_GRAYSCALE)
-cv2.imshow('', img)
+img0 = cv2.imread("./videos/fringes_9/frames/fringes_9-400-.png", cv2.IMREAD_GRAYSCALE)
+cv2.imshow('', img0)
 cv2.waitKey()
+
+img1 = cv2.imread("./videos/fringes_9/frames/fringes_9-369-.png", cv2.IMREAD_GRAYSCALE)
+cv2.imshow('', img1)
+cv2.waitKey()
+
+
+img0_norm = img0/np.sqrt(np.sum(img0**2)) * 255
+img1_norm = img1/np.sqrt(np.sum(img1**2)) * 255
+
+cv2.imshow('', img0_norm)
+cv2.waitKey()
+
+cv2.imshow('', img1_norm)
+cv2.waitKey()
+
+
 cv2.destroyAllWindows()
 sys.exit()
-
-import numpy as np
-
-picture1 = np.random.rand(100,100)
-picture2 = np.random.rand(100,100)
-picture1_norm = picture1/np.sqrt(np.sum(picture1**2))
-picture2_norm = picture2/np.sqrt(np.sum(picture2**2))
