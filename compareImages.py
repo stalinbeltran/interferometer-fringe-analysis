208e22553a0d31fe6a49ee3be1b1c7288ea30c0f
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-13 12:47:10 -0500

    similaritiy:  151

--- compareImages.py
+++ compareImages.py
@@ -4,23 +4,20 @@ import cv2
 import sys
 
 img0 = cv2.imread("./videos/fringes_9/frames/fringes_9-400-.png", cv2.IMREAD_GRAYSCALE)
-cv2.imshow('', img0)
-cv2.waitKey()
+# cv2.imshow('', img0)
+# cv2.waitKey()
 
 img1 = cv2.imread("./videos/fringes_9/frames/fringes_9-369-.png", cv2.IMREAD_GRAYSCALE)
-cv2.imshow('', img1)
-cv2.waitKey()
+# cv2.imshow('', img1)
+# cv2.waitKey()
 
 
-img0_norm = img0/np.sqrt(np.sum(img0**2)) * 255
-img1_norm = img1/np.sqrt(np.sum(img1**2)) * 255
+img0_norm = img0/np.sqrt(np.sum(img0**2))
+img1_norm = img1/np.sqrt(np.sum(img1**2))
 
-cv2.imshow('', img0_norm)
-cv2.waitKey()
+similaritiy = np.sum(img0_norm*img1_norm)
 
-cv2.imshow('', img1_norm)
-cv2.waitKey()
+print("similaritiy: ", similaritiy)
 
 
 cv2.destroyAllWindows()
-sys.exit()
