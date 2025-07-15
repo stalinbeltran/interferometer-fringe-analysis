e3708484ecfaf793545f8677a5b36ef50ba38fd2
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-14 20:39:46 -0500

    run(command)

--- procesadorVideos.py
+++ procesadorVideos.py
@@ -1,4 +1,4 @@
-#python imageVerticalCentralCropper.py 800  ./originales ./recortadas
+#python3 /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/procesadorVideos.py /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos 39-40
 
 import os
 import sys
@@ -9,7 +9,24 @@ inicio, final = rangoArchivos.split('-')
 inicio = int(inicio)
 final = int(final)
 
+def run(command, falseFileName, trueFileName):
+    command = command.replace(falseFileName, trueFileName)
+    os.system(command)
+
+
 for index in range(inicio, final):
+    #h264 to mp4
     command = "ffmpeg -i /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos/fringes_XXX.h264 -c copy /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos/fringes_XXX.mp4"
+    run(command, "fringes_XXX", "fringes_" + str(index))
+    sys.exit(9)
+
+    #video to frames:
+    #create the required folder for the frames
+    command = "mkdir /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos/fringes_XXX/frames"
     command = command.replace("fringes_XXX", "fringes_" + str(index))
     os.system(command)
+    #get frames from mp4
+    command = "ffmpeg -i /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos/fringes_XXX/fringes_XXX.mp4 -vf fps=6 /mnt/d/Stalin/Desarrollo/interferometer-fringe-analysis/videos/fringes_XXX/frames/fringes_XXX-%d-.png"
+    command = command.replace("fringes_XXX", "fringes_" + str(index))
+    os.system(command)
+
