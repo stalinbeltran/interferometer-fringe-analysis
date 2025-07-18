11b88d9648b2bdccf4fecd63d983054dbe6dec92
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-18 13:47:16 -0500

    .

--- compareImages.py
+++ compareImages.py
@@ -1,4 +1,4 @@
-#python3 compareImages.py ./videos/fringes_65/frames/ compareImages.py ./videos/fringes_65/frames/fringes_65-1-.png 30000
+#python3 compareImages.py ./videos/fringes_65/frames/ ./videos/fringes_65/frames/fringes_65-1-.png 30000
 
 import numpy as np
 import cv2
@@ -8,7 +8,7 @@ import imageSineFit as isf
 
 input_folder = (sys.argv[1])
 img0_path = (sys.argv[2])
-threshold = int(sys.argv[3])
+threshold = float(sys.argv[3])
 
 img0 = cv2.imread(img0_path, cv2.IMREAD_GRAYSCALE)
 
@@ -19,9 +19,19 @@ for filename in os.listdir(input_folder):
         if isf.isBlackImage(img1):
             continue                #ignore black images
         diff = np.absolute(img0-img1)
+        
+        print(img0)
+        print(img1)
         difference = np.sum(diff)
+        print(diff)
+        cv2.imshow('frame', diff)
+        break
+        print("archivo: ", filename, " - ", difference)
         if difference < threshold:
             print("archivo: ", filename, " - ", difference)
+            break
+            
 
+cv2.waitKey()
 
 cv2.destroyAllWindows()
