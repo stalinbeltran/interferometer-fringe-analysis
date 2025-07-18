d1055db88f15daeba58e0467e0d5fa2b624ab3e0
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-18 15:04:18 -0500

    output path

--- blurImages.py
+++ blurImages.py
@@ -13,8 +13,11 @@ product = dim*dim
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         inputPath = os.path.join(input_folder, filename)
+        filenameNoExt, file_extension = os.path.splitext(filename)
         img1 = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
         kernel = np.ones((dim,dim),np.float32)/product
         imgBlurred = cv2.filter2D(img1,-1,kernel)
-        cv2.imwrite(filename, imgBlurred)
+        outputPath = os.path.join(input_folder, filenameNoExt + "-blurred" + file_extension)
+        #outputPath = os.path.join(input_folder, filename)
+        cv2.imwrite(outputPath, imgBlurred)
 cv2.destroyAllWindows()
