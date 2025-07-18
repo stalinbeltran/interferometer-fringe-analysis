5d339d753731ceda2a247e6bc231e768f42f7979
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-18 14:09:07 -0500

    works

--- compareImages.py
+++ compareImages.py
@@ -1,4 +1,4 @@
-#python3 compareImages.py ./videos/fringes_65/frames/ ./videos/fringes_65/frames/fringes_65-1-.png 30000
+#python3 compareImages.py ./videos/fringes_65/frames/ ./videos/fringes_65/frames/fringes_65-483-.png 25505474
 
 import numpy as np
 import cv2
@@ -19,19 +19,19 @@ for filename in os.listdir(input_folder):
         if isf.isBlackImage(img1):
             continue                #ignore black images
         diff = np.absolute(img0-img1)
-        
-        print(img0)
-        print(img1)
         difference = np.sum(diff)
-        print(diff)
-        cv2.imshow('frame', diff)
-        break
-        print("archivo: ", filename, " - ", difference)
+        if difference == 0: continue
+        # print(img0)
+        # print(img1)
+        # print(diff)
+        # cv2.imshow('frame', diff)
+        # break
+        # print("archivo: ", filename, " - ", difference)
         if difference < threshold:
             print("archivo: ", filename, " - ", difference)
-            break
+            cv2.imshow('frame', diff)
+            cv2.waitKey()
+            #break
             
-
-cv2.waitKey()
 
 cv2.destroyAllWindows()
