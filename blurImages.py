9d5b75fe0c0aee751cc829c5d53c8c11f32a7c67
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-18 14:54:34 -0500

    blur images

---
+++ blurImages.py
@@ -0,0 +1,20 @@
+#python3 compareImages.py ./videos/fringes_65/frames/ ./videos/fringes_65/frames/fringes_65-483-.png 25505474
+
+import numpy as np
+import cv2
+import os
+import sys
+import imageSineFit as isf
+
+input_folder = (sys.argv[1])
+dim = 5
+product = dim*dim
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        inputPath = os.path.join(input_folder, filename)
+        img1 = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
+        kernel = np.ones((dim,dim),np.float32)/product
+        imgBlurred = cv2.filter2D(img1,-1,kernel)
+        cv2.imwrite(filename, imgBlurred)
+cv2.destroyAllWindows()
