d895d35f0b914b0582c61fac3056b4f048d4583f
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-18 13:31:58 -0500

    compare by difference

--- compareImages.py
+++ compareImages.py
@@ -1,3 +1,4 @@
+#python3 compareImages.py ./videos/fringes_65/frames/ compareImages.py ./videos/fringes_65/frames/fringes_65-1-.png 30000
 
 import numpy as np
 import cv2
@@ -7,23 +8,20 @@ import imageSineFit as isf
 
 input_folder = (sys.argv[1])
 img0_path = (sys.argv[2])
-threshold = float(sys.argv[3])
+threshold = int(sys.argv[3])
 
 img0 = cv2.imread(img0_path, cv2.IMREAD_GRAYSCALE)
-img0_norm = img0/np.sum(img0)
 
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         inputPath = os.path.join(input_folder, filename)
-        filenameNoExt, file_extension = os.path.splitext(filename)
         img1 = cv2.imread(inputPath, cv2.IMREAD_GRAYSCALE)
         if isf.isBlackImage(img1):
             continue                #ignore black images
-
-        img1_norm = img1/np.sum(img1)
-        similarity = np.sum(np.sqrt(img0_norm*img1_norm))
-        if similarity > threshold:
-            print("archivo: ", filename, " - ", similarity)
+        diff = np.absolute(img0-img1)
+        difference = np.sum(diff)
+        if difference < threshold:
+            print("archivo: ", filename, " - ", difference)
 
 
 cv2.destroyAllWindows()
