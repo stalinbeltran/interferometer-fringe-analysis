1a8ab596dfbfec71891364c5bf666c2ae0c2b555
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-20 16:00:57 -0500

    redis publisher consumer

--- redisTest.py
+++ redisTest.py
@@ -39,48 +39,7 @@ getted = r.get('foo')
 print(getted)
 safe_string_base64 = base64.b64decode(getted)
 
-b = np.frombuffer(safe_string_base64, dtype=np.uint16) # or dtype=np.dtype('<f4'), or np.float32 on a little-endian system (which most computers are these days)
+b = np.frombuffer(safe_string_base64, dtype=np.uint16)
 print (b)
 b = b.reshape(8, 8)
 print (b)
-
-# Or, if you want big-endian:
-
-# >>> np.frombuffer(b'\x00\x00\x80?\x00\x00\x00@\x00\x00@@\x00\x00\x80@', dtype='>f4') # or dtype=np.dtype('>f4'), or np.float32  on a big-endian system
-# array([  4.60060299e-41,   8.96831017e-44,   2.30485571e-41,
-         # 4.60074312e-41], dtype=float32)
-
-
-
-# 
-# print('safe_string_base64')
-# print(safe_string_base64)
-
-
-
-
-
-
-
-
-import struct
-import redis
-
-def toRedis(r,a,n):
-   """Store given Numpy array 'a' in Redis under key 'n'"""
-   h, w = a.shape
-   shape = struct.pack('>II',h,w)
-   print('shape', shape)
-   encoded = shape + a.tobytes()
-
-   # Store encoded data in Redis
-   r.set(n,encoded)
-   return
-
-def fromRedis(r,n):
-   """Retrieve Numpy array from Redis key 'n'"""
-   encoded = r.get(n)
-   h, w = struct.unpack('>II',encoded[:8])
-   # Add slicing here, or else the array would differ from the original
-   a = np.frombuffer(encoded[8:]).reshape(h,w)
-   return a
