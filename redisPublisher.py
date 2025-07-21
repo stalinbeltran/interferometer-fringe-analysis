406b1e4fe9b82ca9200326674fbed0151f53169c
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-21 12:08:18 -0500

    work, but redis server maybe the source of the intermittent problem

--- redisPublisher.py
+++ redisPublisher.py
@@ -1,13 +1,15 @@
 import redis
+import time
 
 # initializing the redis instance
 r = redis.Redis(
-    host='127.0.0.1',
+    host='localhost',
     port=6379,
     decode_responses=True # <-- this will ensure that binary data is decoded
 )
 
-
+time.sleep(3)
+r.publish("phototaken", 'message')
 while True:
     message = input("Enter the message you want to send to solders: ")
     r.publish("phototaken", message)
