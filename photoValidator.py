5cc0c69fbc7a8301b81211b193375188536c5639
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-21 13:32:20 -0500

    .

--- photoValidator.py
+++ photoValidator.py
@@ -14,16 +14,11 @@ time.sleep(3)
 for message in pub.listen():
     value = message['data']
     if value == 'qc': exit()
-    print(type(value))
     if isinstance(value, int) or len(value) < 200: continue
     imageBase64 = value
     img = pub.getImage(imageBase64)
-    print(type(img))
     img = img.reshape((480, 640))
-    print('shape')
-    print(img.shape)
-    print(img)
-    cv2.imshow('', img)
-    key = cv2.waitKey()
-    break
+    # cv2.imshow('', img)
+    # key = cv2.waitKey()
+    break                               #debugging
     
