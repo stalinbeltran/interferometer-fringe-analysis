a78977edb62787fe84b1d6cd1550bbf042110ea4
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-21 16:00:12 -0500

    .

--- controller.py
+++ controller.py
@@ -1,10 +1,11 @@
 #python3 ./controller.py
+import controller
 import time
 import numpy as np
 from publisher import Publisher
 import cv2 as cv2
 
-#statuses
+#states
 WAITING_MOBILEMIRROR_PHOTO = 1
 WAITING_FIXEDMIRROR_PHOTO = 2
 
@@ -25,19 +26,19 @@ pub.init()
 print(pub)
 pub.subscribe('photovalidated')
 pub.publish("commandShutter", "hide")          #hide fixed retroreflector
-status = WAITING_MOBILEMIRROR_PHOTO
+state = WAITING_MOBILEMIRROR_PHOTO
 
 mobileMirrorPhoto = None
 fixedMirrorPhoto = None
 for message in pub.listen():
     photo = waitPhoto(message)
-    if not photo: continue
-    match status:
-        case WAITING_MOBILEMIRROR_PHOTO:
+    if photo is None: continue
+    match state:
+        case controller.WAITING_MOBILEMIRROR_PHOTO:
             mobileMirrorPhoto = photo
             pub.publish("commandShutter", "show")          #hide fixed retroreflector
-            status = WAITING_FIXEDMIRROR_PHOTO
-        case WAITING_FIXEDMIRROR_PHOTO:
+            state = WAITING_FIXEDMIRROR_PHOTO
+        case controller.WAITING_FIXEDMIRROR_PHOTO:
             fixedMirrorPhoto = photo
             data = {
                 "mobileMirrorPhoto": pub.imageToString(mobileMirrorPhoto),
@@ -46,7 +47,7 @@ for message in pub.listen():
             dataString = json.dumps(data)
             #save data
             pub.publish("saveDataPair", dataString)
-            status = WAITING_MOBILEMIRROR_PHOTO
+            state = WAITING_MOBILEMIRROR_PHOTO
             
 
     break                               #debugging
