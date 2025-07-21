afeedc5ebd2660264b098d08d6e77eda83723050
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-20 19:27:25 -0500

    consumer

---
+++ redisConsumer.py
@@ -0,0 +1,21 @@
+
+import redis
+
+r = redis.Redis(
+    host='127.0.0.1',
+    port=6379,
+    decode_responses=True
+)
+
+# pubsub() method creates the pubsub object
+
+mobile = r.pubsub()
+
+# use .subscribe() method to subscribe to topic on which you want to listen for messages
+mobile.subscribe('army-camp-1')
+
+# .listen() returns a generator over which you can iterate and listen for messages from publisher
+
+for message in mobile.listen():
+    print(message) # <-- you can literally do any thing with this message i am just printing it
+
