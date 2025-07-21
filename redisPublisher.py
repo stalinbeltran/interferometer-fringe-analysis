11d9b5d11803c5570a9286a0bcc1161b227e6f85
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-20 20:21:53 -0500

    works

--- redisPublisher.py
+++ redisPublisher.py
@@ -2,7 +2,7 @@ import redis
 
 # initializing the redis instance
 r = redis.Redis(
-    host='127.0.0.1',
+    host='localhost',
     port=6379,
     decode_responses=True # <-- this will ensure that binary data is decoded
 )
@@ -10,4 +10,5 @@ r = redis.Redis(
 
 while True:
     message = input("Enter the message you want to send to solders: ")
+    if message == 'q': break
     r.publish("army-camp-1", message)
