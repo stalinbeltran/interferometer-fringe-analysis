594cd8f920c0f1b56966c15aacc94efcbb98d9f6
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-22 17:21:26 -0500

    .

--- globals.py
+++ globals.py
@@ -1,10 +1,28 @@
 
-#import msvcrt
-import sys, tty, termios
-import select
+import os
+OS = my_variable_value = os.getenv('OS_INTERFEROMETER_FRINGE_ANALYSIS')
+
+if OS == "WINDOWS":
+    import msvcrt
+    
+    def getKey():
+        return getKey_WINDOWS()
+
+if OS == "LINUX":
+    import sys, tty, termios
+    import select
+    
+    def getKey():
+        return getKey_UNIX()
+    
+    
+    
+import numpy as np
 
 #SERIAL_PORT_NAME = 'COM3'
 SERIAL_PORT_NAME = '/dev/ttyUSB0'
+
+BLACK_IMAGE_LEVEL = 10
 
 #states
 WAITING_MOBILE_MIRROR_PHOTO = 1
@@ -14,9 +32,15 @@ WAITING_FIXED_MIRROR_PHOTO = 2
 HIDE = "hide"
 SHOW = "show"
 
+#photo taken size
+WIDTH = 1280
+HEIGHT = 800
 
-def getKey():
-    return getKey_UNIX()
+
+#photo resized size
+RESIZED_WIDTH = 320
+RESIZED_HEIGHT = 200
+
 
 def getKey_WINDOWS():
     if msvcrt.kbhit():  # Check if a keypress is available
@@ -29,15 +53,31 @@ def getKey_UNIX():
     try:
         tty.setcbreak(fd)  # Set terminal to cbreak mode (non-canonical)
         if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            print('getKey_UNIX 1')
             return sys.stdin.read(1)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return None
     
-def shouldCloseThisApp():
-    key = getKey()
-    if key == 'q':
-        print("salir")
+def isPressedKey(key):
+    keyReaded = getKey()
+    if keyReaded: print(keyReaded)
+    if keyReaded == key:
         return True
     return False
+    
+    
+    
+def shouldCloseThisApp():
+    return isPressedKey('q')
+    
+def shouldPauseThisApp():
+    print('should pause check')
+    return isPressedKey('p')
+    
+def toY8array(Y16array, width, height):
+    Y16array = Y16array.reshape((width*height, 2))
+    Y8array = np.ascontiguousarray(Y16array[:, 1])                     #avoid unnecesary zeroes, halve the size
+    Y8array = Y8array.reshape((height, width))
+    return Y8array
     
