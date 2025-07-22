fba9e32a0fd5b90add937c726675f33a7ed0e4fb
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-22 11:29:06 -0500

    not showing until close window

--- photoValidator.py
+++ photoValidator.py
@@ -11,18 +11,20 @@ pub.init(hostIP='192.168.0.24')
 print(pub)
 pub.subscribe('phototaken')
 
+c=0
 while True:
     if globals.shouldCloseThisApp(): break
     message = pub.get_message()
     if message is None: continue
     value = message['data']
     if isinstance(value, int) or len(value) < 200: continue
-    print('image')
+    c +=1
+    print('image' + str(c))
     imageBase64 = value
     photo = pub.getImage(imageBase64, globals.WIDTH, globals.HEIGHT)
     pub.publishImage("photovalidated", photo)
     cv2.imshow('', photo)
-    #key = cv2.waitKey()
+    cv2.waitKey()
 
     
 cv2.destroyAllWindows()
