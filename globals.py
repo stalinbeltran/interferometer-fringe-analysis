6fb6b271443a47f63052ae94b0dfb3b2864db015
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-21 19:10:06 -0500

    get key unix

--- globals.py
+++ globals.py
@@ -1,5 +1,6 @@
 
-import msvcrt
+#import msvcrt
+import sys, tty, termios
 
 #states
 WAITING_MOBILE_MIRROR_PHOTO = 1
@@ -11,8 +12,22 @@ SHOW = "show"
 
 
 def getKey():
+    return getKey_UNIX()
+
+def getKey_WINDOWS():
     if msvcrt.kbhit():  # Check if a keypress is available
         return msvcrt.getch().decode() # Read the character
+    return None
+    
+def getKey_UNIX():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setcbreak(fd)  # Set terminal to cbreak mode (non-canonical)
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            return sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return None
     
 def shouldCloseThisApp():
