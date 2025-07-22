0fabb5ba10f98914c194b61f4fa9d02866c1b227
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-22 07:15:10 -0500

    to8bits

--- arrayTest.py
+++ arrayTest.py
@@ -5,10 +5,12 @@ a = np.array([[ 0, 40,  0,  52,  0, 55],
     [ 0, 41,  0,  53,  0, 53],
 ])
 
+def to8bits(a):
+    a = a.reshape((6, 2))
+    a = a[:, 1]
+    return a
+    
+
 print(a)
-a = a.reshape((12))
-print(a)
-a = a.reshape((6, 2))
-print(a)
-a = a[:, 1]
+a = to8bits(a)
 print(a)
