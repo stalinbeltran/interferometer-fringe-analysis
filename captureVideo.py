3a1fcd9e579bec86e793c120616b2fc88e9e4d0e
stalinbeltran <stalin.beltran2006@gmail.com>
2025-07-28 17:11:39 -0500

    debugging

--- captureVideo.py
+++ captureVideo.py
@@ -32,30 +32,40 @@ picam2.start()
 picam2.set_controls({'ExposureTime':200})
 
 c = 0
+goodOnes= 0
+lastOne = 0
 
 def capture():
-    global picam2, c
-    key = globals.getKey()
-    if globals.shouldCloseThisApp(key):
-        picam2.stop()
-        exit()
-        # ~ valid = True
-        # ~ print('q received')
-
-    request = picam2.capture_request(flush = True)
+    global picam2, c, goodOnes
+    lastOne = time.monotonic_ns()
+    request = picam2.capture_request(flush = True) #6000
     photo = request.make_array('raw')                 #photo have 16 bits when actually 8 bits where sent by the camera
     request.release()
     photo = globals.toY8array(photo, WIDTH, HEIGHT)     #so we fix that
     # ~ cv2.imshow('sample', photo)
     # ~ cv2.waitKey(1)
     c +=1
-    if c % 10 == 0: print('image' + str(c))
-
+    if c % 10 == 0:
+        # ~ print('image' + str(c))
+        print('goodOnes: ' + str(goodOnes))
+        goodOnes = 0
+    # ~ isf.isBlackImage(photo)
     if not isf.isBlackImage(photo):
-        cv2.imshow('sample', photo)
-        cv2.waitKey(1)
+        goodOnes +=1
+        now = time.monotonic_ns()
+        print('image: ' + str(c) + ' - elapsed: ' + str((now - lastOne)/1000))
+        lastOne = now
+        # ~ cv2.imshow('sample', photo)
+        # ~ cv2.waitKey(1)
         # ~ pub.publishImage(globals.FOTO_TAKEN, photo)                   #original for files
         # ~ pub.publishImage(globals.FOTO_TAKEN_RESIZED, resized_image)    #resized for fast feedback
+
+    key = globals.getKey()
+    if globals.shouldCloseThisApp(key):
+        picam2.stop()
+        exit()
+        # ~ valid = True
+        # ~ print('q received')
 
 
 
