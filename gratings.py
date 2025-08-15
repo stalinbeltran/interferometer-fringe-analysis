21414492a48c7ba862dbadc704d9a0f3812a0ab2
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-15 17:01:02 -0500

    .

--- gratings.py
+++ gratings.py
@@ -1,11 +1,13 @@
 # gratings.py
 
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 
+
+wavelength = int(sys.argv[1])
 x = np.arange(-500, 501, 1)
 
-wavelength = 200
 y = np.sin(2 * np.pi * x / wavelength)
 
 plt.plot(x, y)
