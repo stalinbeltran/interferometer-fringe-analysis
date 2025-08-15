81a82c941d7ff120ba4e2920869d4961b867762c
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-15 17:06:10 -0500

    2D vertical sine

--- gratings.py
+++ gratings.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 wavelength = int(sys.argv[1])
 x = np.arange(-500, 501, 1)
-
-y = np.sin(2 * np.pi * x / wavelength)
-
-plt.plot(x, y)
+X, Y = np.meshgrid(x, x)
+grating = np.sin(2 * np.pi * X / wavelength)
+plt.set_cmap("gray")
+plt.imshow(grating)
 plt.show()
