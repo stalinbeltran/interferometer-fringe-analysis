911fefe5445f2f29d787d46fbfa3e3a602d1d676
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-15 17:37:49 -0500

    .

--- gratings.py
+++ gratings.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 
 
 wavelength = int(sys.argv[1])
+angleFraction = int(sys.argv[2])
+
 x = np.arange(-500, 501, 1)
 X, Y = np.meshgrid(x, x)
 
-angle = np.pi / 9
+angle = np.pi / angleFraction
 grating = np.sin(
     2*np.pi*(X*np.cos(angle) + Y*np.sin(angle)) / wavelength
 )
