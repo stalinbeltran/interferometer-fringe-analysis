689d8c20bd6180424625cd5d87c1448e1f9ab864
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-15 17:14:48 -0500

    angled fringes

--- gratings.py
+++ gratings.py
@@ -8,7 +8,12 @@ import matplotlib.pyplot as plt
 wavelength = int(sys.argv[1])
 x = np.arange(-500, 501, 1)
 X, Y = np.meshgrid(x, x)
-grating = np.sin(2 * np.pi * Y / wavelength)
+
+angle = np.pi / 9
+grating = np.sin(
+    2*np.pi*(X*np.cos(angle) + Y*np.sin(angle)) / wavelength
+)
+
 plt.set_cmap("gray")
 plt.imshow(grating)
 plt.show()
