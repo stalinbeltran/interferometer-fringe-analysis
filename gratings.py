67f18632899d309341f87d676f6787139ec31a33
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-15 17:07:08 -0500

    horizontal fringes

--- gratings.py
+++ gratings.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 wavelength = int(sys.argv[1])
 x = np.arange(-500, 501, 1)
 X, Y = np.meshgrid(x, x)
-grating = np.sin(2 * np.pi * X / wavelength)
+grating = np.sin(2 * np.pi * Y / wavelength)
 plt.set_cmap("gray")
 plt.imshow(grating)
 plt.show()
