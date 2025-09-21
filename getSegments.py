89402c8f58a728b112084aec17a5649983ff3e6b
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-21 15:04:15 -0500

    .

--- getSegments.py
+++ getSegments.py
@@ -9,32 +9,23 @@ input_file = (sys.argv[1])
 output_file = (sys.argv[2])
 
 with open(input_file, 'r', encoding='utf-8') as f:
-    files = json.load(files, f)
+    files = json.load(f)
 
 TMAX = 2*(1/(RMPMAX/60))
 
 segments = []           #list of segments
-segment = {
-    "files": [],        #array of all its files
-    "timestamp" None    #the timestamp of its first file, as a reference
-    }
-segments.append(segment)    #add the first segment
 previousTimestamp = None
 for file in files:
     timestamp = file["timestamp"]
-    if previousTimestamp is None:                   #this is the first file
-        segment["files"].append(file)                #always add this file to actual segment (it should belong to one segment at least)
-        segment["timestamp"] = timestamp            #this segment will have the timestamp of its first file
-        previousTimestamp = timestamp
-        continue
+    if previousTimestamp is None:                   #this is the first file         
+        previousTimestamp = timestamp               #previousTimestamp initial value 
     if timestamp - previousTimestamp > TMAX:        #this is a new segment
         segment = {"files": [], "timestamp" None}
-        segment["files"].append(file)                #always add this file to actual segment (it should belong to one segment at least)
-        segment["timestamp"] = timestamp
+        segment["timestamp"] = timestamp            #this segment will have the timestamp of its first file
         segments.append(segment)                    #add the new segment
 
     previousTimestamp = timestamp
-    segment["files"].append(file)
+    segment["files"].append(file)                   #always add this file to actual segment (it should belong to one segment at least)
 
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(segments, f, ensure_ascii=False, indent=4)
