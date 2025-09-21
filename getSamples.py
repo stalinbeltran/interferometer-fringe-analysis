bf4167571b7640000de0ecad31c02dfe35d37b47
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-21 16:41:19 -0500

    .

---
+++ getSamples.py
@@ -0,0 +1,35 @@
+ #python3 getSamples.py ./60hzSegments.json ./60hzSegmentSamples.json
+
+import os
+import sys
+import json
+
+RMPMAX = 400
+input_file = (sys.argv[1])
+output_file = (sys.argv[2])
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    segments = json.load(f)
+
+TMAX = 2*(1/(RMPMAX/60))
+
+segmentSamples = []
+previousTimestamp = None
+
+for segment in segments:
+    segmentSamples.append(segmentSamples)
+    for file in files:
+        timestamp = file["timestamp"]
+        if previousTimestamp is None:                   #this is the first file         
+            previousTimestamp = timestamp               #previousTimestamp initial value 
+        if float(timestamp) - float(previousTimestamp) > TMAX:        #this is a new segment
+            segment = {"files": [], "timestamp": None}
+            segment["timestamp"] = timestamp            #this segment will have the timestamp of its first file
+            segments.append(segment)                    #add the new segment
+
+        previousTimestamp = timestamp
+        segment["files"].append(file)                   #always add this file to actual segment (it should belong to one segment at least)
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(segments, f, ensure_ascii=False, indent=4)
+
