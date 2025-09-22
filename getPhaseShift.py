e46b34661c708078dff0c6e37bd3311641f6c03f
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-22 16:09:12 -0500

    try except for None values

--- getPhaseShift.py
+++ getPhaseShift.py
@@ -17,16 +17,22 @@ segments = segmentsJSON["segments"]
 for segment in segments:
     samples = segment["samples"]
     for sample in samples:
+        deltaPhase = None
+        averagePeriod = None
+        deltaPhasePixels = None
         fileMobile = sample["fileMobileMirror"]
         fileFixed = sample["fileFixedMirror"]
-        deltaPhase = (fileMobile["fftParams"]["phase"] - fileFixed["fftParams"]["phase"])/360
-        averagePeriod = (fileMobile["fftParams"]["period"] + fileFixed["fftParams"]["period"])/2
-        deltaPhasePixels = averagePeriod*deltaPhase
+        try:
+            deltaPhase = (fileMobile["fftParams"]["phase"] - fileFixed["fftParams"]["phase"])/360       #phase in degrees, converted to fractional(0.0-1.0)
+            averagePeriod = (fileMobile["fftParams"]["period"] + fileFixed["fftParams"]["period"])/2    #in case there are differences in period
+            deltaPhasePixels = averagePeriod*deltaPhase                                                 #useful to compare samples by pixels
+        except Exception as e:
+            print(e)
+
         sample["deltaPhase"] = deltaPhase
         sample["averagePeriod"] = averagePeriod
         sample["deltaPhasePixels"] = deltaPhasePixels
-        break
-    break
+
 
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
