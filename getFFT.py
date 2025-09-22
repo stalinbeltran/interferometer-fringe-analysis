7d13852e386556f6cee7e0114b180131e80d37b9
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-22 10:50:44 -0500

    .

---
+++ getFFT.py
@@ -0,0 +1,60 @@
+ #python3 getSamples.py ./60hzSegments.json ./60hzSegmentSamples.json
+
+import os
+import sys
+import json
+
+DISTANCE_FACTOR = 0.5
+input_file = (sys.argv[1])
+output_file = (sys.argv[2])
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    segmentsJSON = json.load(f)
+
+segments = segmentsJSON["segments"]
+TMAX_SAMPLE = None
+
+segmentsNew = []
+previousTimestamp = None
+
+for segment in segments:
+    files = segment["files"]
+    if len(files) < 3:
+        print("Files not used:")
+        print(files)
+        continue                 #we need al least 3 files
+    previousPeriod = float(files[2]["timestamp"]) - float(files[0]["timestamp"])
+    TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
+    previousTimestamp = None
+    previousFile = None
+    previousSampleTimestamp = None
+    segmentNew = {"timestamp": None, "samples": []}
+    segmentsNew.append(segmentNew)
+    samples = []
+    segmentNew["timestamp"] = segment["timestamp"]            #this segment will have the timestamp of its first file
+    segmentNew["samples"] = samples
+    for file in files:
+        timestamp = file["timestamp"]
+        if previousTimestamp is None:                   #this is the first file         
+            previousTimestamp = timestamp               #previousTimestamp initial value 
+            previousFile = file
+            
+            if previousSampleTimestamp:
+                previousPeriod = float(timestamp) - float(previousSampleTimestamp)
+                TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
+            continue            #wait for the next file
+            
+        if float(timestamp) - float(previousTimestamp) <= TMAX_SAMPLE:        #this is a new sample
+            sample = {"timestamp": previousTimestamp, "fileMobileMirror": previousFile, "fileFixedMirror": file}
+            samples.append(sample)
+            previousSampleTimestamp = previousTimestamp #saved to calculate Period with the next file
+            previousFile = None                         #this file has now been used
+            previousTimestamp = None
+        else:
+            previousTimestamp = timestamp               #this is now the previous timestamp
+            previousFile = file                         #this is the previous file
+
+segmentsJSON = {"segments": segmentsNew}
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
+
