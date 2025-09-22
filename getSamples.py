ff98cb2b00edd3e9c0f3f350b3fb17073346a76b
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-22 09:45:53 -0500

    .

--- getSamples.py
+++ getSamples.py
@@ -4,7 +4,7 @@ import os
 import sys
 import json
 
-DISTANCE_FACTOR = 0.3
+DISTANCE_FACTOR = 0.5
 input_file = (sys.argv[1])
 output_file = (sys.argv[2])
 
@@ -22,14 +22,14 @@ sampleCounter = 0
 for segment in segments:
     files = segment["files"]
     if len(files) < 3:
-        print("Files not used:")
-        print(files)
+        #print("Files not used:")
+        #print(files)
         continue                 #we need al least 3 files
     previousPeriod = float(files[2]["timestamp"]) - float(files[0]["timestamp"])
     TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
     
-    print("previousPeriod:", previousPeriod)
-    print("TMAX_SAMPLE:", TMAX_SAMPLE)
+    #print("previousPeriod:", previousPeriod)
+    #print("TMAX_SAMPLE:", TMAX_SAMPLE)
     previousTimestamp = None
     previousFile = None
     previousSampleTimestamp = None
@@ -41,7 +41,7 @@ for segment in segments:
     for file in files:
         fileCounter+=1
         timestamp = file["timestamp"]
-        print("timestamp:", timestamp)
+        #print("timestamp:", timestamp)
         if previousTimestamp is None:                   #this is the first file         
             previousTimestamp = timestamp               #previousTimestamp initial value 
             previousFile = file
@@ -51,7 +51,10 @@ for segment in segments:
                 TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
             continue            #wait for the next file
         dif = float(timestamp) - float(previousTimestamp)
-        print("dif:", dif)
+        #print("dif:", dif)
+        
+        #print("previousPeriod:", previousPeriod)
+        #print("TMAX_SAMPLE:", TMAX_SAMPLE)
         if dif <= TMAX_SAMPLE:        #this is a new sample
             sampleCounter+=1
             sample = {"timestamp": previousTimestamp, "fileMobileMirror": previousFile, "fileFixedMirror": file}
@@ -59,7 +62,7 @@ for segment in segments:
             previousSampleTimestamp = previousTimestamp #saved to calculate Period with the next file
             previousFile = None                         #this file has now been used
             previousTimestamp = None
-        if fileCounter > 10: break
+        #if fileCounter > 10: break
     if fileCounter > 2*sampleCounter + 2:
         print("fileCounter: ", fileCounter)
         print("sampleCounter: ", sampleCounter)
