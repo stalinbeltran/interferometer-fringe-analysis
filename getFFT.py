1c370773dd1114087670483672460c2844a4f8a2
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-09-22 11:08:56 -0500

    .

--- getFFT.py
+++ getFFT.py
@@ -4,57 +4,49 @@ import os
 import sys
 import json
 
-DISTANCE_FACTOR = 0.5
 input_file = (sys.argv[1])
 output_file = (sys.argv[2])
+
+def getParameters(image):
+    sizex = image.shape[1]        #number of columns
+    ft = np.fft.ifftshift(image)
+    ft = np.fft.fft2(ft)
+    sample_spacing_x = 1
+    freq_x = np.fft.fftfreq(sizex, d=sample_spacing_x)
+    absft = abs(ft)
+    absftFlat = absft.flatten()
+    sorted_indices = np.argsort(-absftFlat)
+
+    for i in range(0, 6):
+        period = sizex
+        maxPosition = sorted_indices[i]
+        index = np.unravel_index(maxPosition, np.shape(ft))
+        frequency = freq_x[index[1]]
+        if frequency > 0: period = 1/frequency
+        if frequency > 0 and period < sizex/2:
+            imaginaryValue = ft[index]
+            phase = np.angle(imaginaryValue, True)
+            magnitude = absftFlat[i]/1000000
+            print('magnitude: ', magnitude)
+            print('phase: ', phase)
+            print('periodo: ', period)
+            break
+    return magnitude, phase, period
+
 
 with open(input_file, 'r', encoding='utf-8') as f:
     segmentsJSON = json.load(f)
 
 segments = segmentsJSON["segments"]
-TMAX_SAMPLE = None
-
 segmentsNew = []
-previousTimestamp = None
 
 for segment in segments:
     files = segment["files"]
-    if len(files) < 3:
-        print("Files not used:")
-        print(files)
-        continue                 #we need al least 3 files
-    previousPeriod = float(files[2]["timestamp"]) - float(files[0]["timestamp"])
-    TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
-    previousTimestamp = None
-    previousFile = None
-    previousSampleTimestamp = None
-    segmentNew = {"timestamp": None, "samples": []}
-    segmentsNew.append(segmentNew)
-    samples = []
-    segmentNew["timestamp"] = segment["timestamp"]            #this segment will have the timestamp of its first file
-    segmentNew["samples"] = samples
+    t = float(files[2]["timestamp"]) - float(files[0]["timestamp"])
     for file in files:
         timestamp = file["timestamp"]
-        if previousTimestamp is None:                   #this is the first file         
-            previousTimestamp = timestamp               #previousTimestamp initial value 
-            previousFile = file
-            
-            if previousSampleTimestamp:
-                previousPeriod = float(timestamp) - float(previousSampleTimestamp)
-                TMAX_SAMPLE = DISTANCE_FACTOR*previousPeriod
-            continue            #wait for the next file
-            
-        if float(timestamp) - float(previousTimestamp) <= TMAX_SAMPLE:        #this is a new sample
-            sample = {"timestamp": previousTimestamp, "fileMobileMirror": previousFile, "fileFixedMirror": file}
-            samples.append(sample)
-            previousSampleTimestamp = previousTimestamp #saved to calculate Period with the next file
-            previousFile = None                         #this file has now been used
-            previousTimestamp = None
-        else:
-            previousTimestamp = timestamp               #this is now the previous timestamp
-            previousFile = file                         #this is the previous file
 
-segmentsJSON = {"segments": segmentsNew}
+
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
 
