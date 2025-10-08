878668aa9e83940a7814d0e3b16f95daf6af4262
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-08 11:32:07 -0500

    period looked by segment (easy image processing)

--- showPeriods.py
+++ showPeriods.py
@@ -7,21 +7,36 @@ import cv2
 import numpy as np
 import histogram
 import phaseProcessing
+import globals
 
 input_file = (sys.argv[1])
+output_file = (sys.argv[2])
 
-def countPeriods(value):
-    global previousPeriods, periodsCounter
-    if value not in previousPeriods:
-        previousPeriods.append(value)
-        periodsCounter[value] = 1
-    else:
-        periodsCounter[value] += 1
+def countPeriods(value, previousPeriodsCounters):
+    for period in previousPeriodsCounters:
+        if period["period"] == value:
+            periodFound = period
+            periodFound["count"] +=1
+            return
+    periodFound = {"period": value, "count": 1}
+    previousPeriodsCounters.append(periodFound)
+        
+    
 
 
+def getLookedPeriod(previousPeriodsCounters):
+    maximumPeriod = globals.WIDTH/4                 #do not take photos with less than 4 fringes
+    previousPeriodsCountersSorted = sorted(previousPeriodsCounters, key=lambda x: x['count'])
+    for period in previousPeriodsCountersSorted:
+        if period["period"] > maximumPeriod: continue
+        return period["period"]
+    return None
 
 with open(input_file, 'r', encoding='utf-8') as f:
     segmentsJSON = json.load(f)
+
+with open(output_file, 'w', encoding='utf-8') as f:         #verify it is openable
+    pass
 
 segments = segmentsJSON["segments"]
 processed = 0
@@ -30,16 +45,15 @@ periods = []
 for segment in segments:
     samples = segment["samples"]
     segmentPeriod = []
-    previousPeriods = []
-    periodsCounter = {}
+    previousPeriodsCounters = []
     for sample in samples:
         fileMobile = sample["fileMobileMirror"]
         fileFixed = sample["fileFixedMirror"]
         try:
             periods.append(fileMobile["period"]["period"])
-            countPeriods(periods[-1])
+            countPeriods(periods[-1], previousPeriodsCounters)
             periods.append(fileFixed["period"]["period"])
-            countPeriods(periods[-1])
+            countPeriods(periods[-1], previousPeriodsCounters)
         except:
             nulls +=1
             continue
@@ -50,9 +64,15 @@ for segment in segments:
         #if processed > 5: break
     #histogram.showHistogram(segmentPeriod, "segment periods")
     print("------segment timestamp: ", segment["timestamp"])
-    print("periodsCounter: ", periodsCounter)
+    print("previousPeriodsCounters: ", previousPeriodsCounters)
+    segment["periodsCounter"] = {"periodsCounter": previousPeriodsCounters}
+    segment["lookedPeriod"] = getLookedPeriod(previousPeriodsCounters)
     
+    
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
+
 print("nulls: ", nulls)
-histogram.showHistogram(periods, label = ["1", "2"])
+histogram.showHistogram(periods, label = ["Periods"])
 
 
