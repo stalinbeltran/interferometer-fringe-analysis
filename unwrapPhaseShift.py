c3a9ada121bbf2ff1c4417e51f6d124967c205b3
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-10 08:46:20 -0500

    .

--- unwrapPhaseShift.py
+++ unwrapPhaseShift.py
@@ -20,54 +20,52 @@ def lastPointsAverage(lastPoints):
 
 
 
+def unwrapPhaseShift(segmentsJSON, phaseKey):
+    global phaseMaxDifference, distanceImprovementFactor, N_lastPoints
+    segments = segmentsJSON["segments"]
+    processed = 0
+    show = False
+    showed = 0
+    maxShowed = 4
+    for segment in segments:
+        samples = segment["samples"]
+        previousSamplePhase = None
+        lastPoints = deque(maxlen=N_lastPoints)
+        for sample in samples:
+            if phaseKey not in sample: continue
+            phase = sample[phaseKey]
+            timestamp = sample["timestamp"]
+            if not previousSamplePhase:
+                previousSamplePhase = phase
+                continue
+            distance = abs(previousSamplePhase-phase)
+            if distance >= phaseMaxDifference:    #too much error
+                increasedPhaseDistance = abs(previousSamplePhase-(phase + 1))
+                decreasedPhaseDistance = abs(previousSamplePhase-(phase - 1))
+                if show:
+                    print("-----timestamp:", timestamp)
+                    print("phase:", phase)
+                    print("distance:", distance)
+                    print("increasedPhaseDistance:", increasedPhaseDistance)
+                    print("decreasedPhaseDistance:", decreasedPhaseDistance) 
+                if increasedPhaseDistance < distance*distanceImprovementFactor: phase+=1          #if we can make it closer to average, we do
+                elif decreasedPhaseDistance < distance*distanceImprovementFactor: phase-=1
+                sample[phaseKey] = phase
+                processed+=1
+            if show:
+                showed+=1
+            if showed >= maxShowed: exit()
+            lastPoints.append(phase)
+            previousSamplePhase = lastPointsAverage(lastPoints)
+    
+    print("processed: ", processed)
+
 
 with open(input_file, 'r', encoding='utf-8') as f:
     segmentsJSON = json.load(f)
-
-segments = segmentsJSON["segments"]
-inicio = 650
-final = inicio + 1
-processed = 0
-show = False
-showed = 0
-maxShowed = 4
-for segment in segments:
-    samples = segment["samples"]
-    previousSamplePhase = None
-    lastPoints = deque(maxlen=N_lastPoints)
-    for sample in samples:
-        if "deltaPhase" not in sample: continue
-        deltaPhase = sample["deltaPhase"]
-        timestamp = sample["timestamp"]
-        if not previousSamplePhase:
-            previousSamplePhase = deltaPhase
-            continue
-        distance = abs(previousSamplePhase-deltaPhase)
-        if timestamp == "1759504798.269329":
-            show = False
-        if distance >= phaseMaxDifference:    #too much error
-            increasedPhaseDistance = abs(previousSamplePhase-(deltaPhase + 1))
-            decreasedPhaseDistance = abs(previousSamplePhase-(deltaPhase - 1))
-            if show:
-                print("-----timestamp:", timestamp)
-                print("deltaPhase:", deltaPhase)
-                print("distance:", distance)
-                print("increasedPhaseDistance:", increasedPhaseDistance)
-                print("decreasedPhaseDistance:", decreasedPhaseDistance) 
-            if increasedPhaseDistance < distance*distanceImprovementFactor: deltaPhase+=1          #if we can make it closer to average, we do
-            elif decreasedPhaseDistance < distance*distanceImprovementFactor: deltaPhase-=1
-            sample["deltaPhase"] = deltaPhase
-            processed+=1
-        if show:
-            print("final deltaPhase:", deltaPhase)
-            showed+=1
-        if showed >= maxShowed: exit()
-            #if processed >= final: break
-        lastPoints.append(deltaPhase)
-        previousSamplePhase = lastPointsAverage(lastPoints)
+unwrapPhaseShift(segmentsJSON, "deltaPhase")
 
 
-print("processed: ", processed)
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
 
