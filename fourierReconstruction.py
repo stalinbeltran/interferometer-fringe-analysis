db5567eeedcdbe4b2f87bbabb7501e37a55480d9
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-11 10:48:30 -0500

    .

--- fourierReconstruction.py
+++ fourierReconstruction.py
@@ -1,21 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def filter(signal, cutoff = 25):
+    fourier_transform = np.fft.fft(signal)
+    fourier_transform[cutoff:-cutoff] = 0       #low pass filter
+    reconstructed_signal = np.fft.ifft(fourier_transform) #Inverse Fourier Transform
+    return reconstructed_signal
+
+
 # 1. Create a sample signal
 # x = np.arange(0, 2*np.pi, 0.1)
 x = np.linspace(0, 2 * np.pi, 100)
-signal = np.sin(3*x) + 0.5 * np.cos(2 * x)
+signal = np.sin(6*x) + 0.5 * np.cos(2 * x) + np.sin(60*x)
 
-# 2. Compute the Fourier Transform
-fourier_transform = np.fft.fft(signal)
-
-# 3. Manipulate the frequency data (e.g., apply a low-pass filter)
-# Keep only the first few frequency components
-cutoff = 5
-fourier_transform[cutoff:-cutoff] = 0
-
-# 4. Perform the Inverse Fourier Transform
-reconstructed_signal = np.fft.ifft(fourier_transform)
+reconstructed_signal = filter(signal)
 
 # 5. Visualize the results
 plt.figure(figsize=(10, 6))
