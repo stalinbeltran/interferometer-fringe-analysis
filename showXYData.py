8074ecf77c1940a12032e4a7502c043c43b7ff11
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-16 11:58:57 -0500

    .

--- showXYData.py
+++ showXYData.py
@@ -1,4 +1,4 @@
-#python3 showXYData.py D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_direct-PlainData.json timestamp mobilePhase
+#python3 showXYData.py D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_direct-PhaseSoften.json hz mobilePhase
 
 
 import os
@@ -14,23 +14,16 @@ input_file = (sys.argv[1])
 xkey = (sys.argv[2])
 ykey = (sys.argv[3])
 
-#sampleSize = int(sys.argv[4])
 
+with open(input_file, 'r', encoding='utf-8') as f:
+    dataJSON = json.load(f)
 
-
-def showFileData(input_file):
-    global sampleSize
-    with open(input_file, 'r', encoding='utf-8') as f:
-        segmentsJSON = json.load(f)
-
-    processed = 0
-    title = input_file
-    segments = segmentsJSON["segments"]
-    for segment in segments:
-        plt.plot(segment[xkey], segment[ykey], '.', label='ykey')
-        plt.title(title)
-        plt.legend()
-        plt.show()
-
-
-showFileData(input_file)
+title = input_file
+softenedArray = dataJSON["softened"]
+noises = []
+for softData in softenedArray:
+    plt.plot(softData["data"][xkey], softData["data"][ykey], '.', label='ykey')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    
