67f78ad6b68628110dd5086b004e31bc4f0b0c28
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-17 13:49:44 -0500

    .

---
+++ getContinuousPhaseCorrection.py
@@ -0,0 +1,59 @@
+#python3 getContinuousPhaseCorrection.py D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_periscope-Continuous.json D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_periscope-PhaseCorrection.json 
+
+
+import os
+import sys
+import json
+import cv2
+import numpy as np
+import globals
+from collections import deque
+import matplotlib.pyplot as plt
+
+input_file = (sys.argv[1])
+output_file = (sys.argv[2])
+#parameters = (sys.argv[3])
+
+
+MAXIMUM_DISTANCE = 0.03
+MINIMUM_SECTION_LENGTH = 3
+CONTINUOUS_FACTOR = 0.8
+    
+
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    dataJSON = json.load(f)
+
+n = 10
+results = dataJSON
+for key in results:
+    previousSection = None
+    for result in results[key]:
+        isContinuous = result["isContinuous"]
+        section = result["section"]
+        result["localPhaseCorrection"] = 0
+        if not isContinuous or len(section) < 50: continue
+        if previousSection:
+            previousSectionAvg = sum(previousSection[-n:])/n
+            sectionAvg = sum(section[:n])/n
+            diff = previousSectionAvg-sectionAvg
+            if abs(diff) > 0.5:
+                if diff > 0: increment = 1
+                else: increment = -1
+                result["localPhaseCorrection"] = increment
+                # print("previousSectionAvg: ", str(previousSectionAvg))
+                # print("sectionAvg: ", str(sectionAvg))
+                # exit(0)
+        previousSection = section
+
+
+
+outputJSON = dataJSON
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(outputJSON, f, ensure_ascii=False, indent=4)
+
+
+
+
+
--- unwrapPhase.py
+++ unwrapPhase.py
@@ -1,4 +1,4 @@
-#python3 unwrapPhase.py D:\Stalin\FotosFranjasProyecto\results\friday03102025-SegmentSamplesPhaseShift.json D:\Stalin\FotosFranjasProyecto\results\friday03102025-PhaseShiftUnwraped.json 
+#python3 unwrapPhase.py D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_periscope-PlainData.json D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_periscope-PhaseCorrection.json D:\Stalin\FotosFranjasProyecto\results\thursday09102025_slowacceleration_periscope-PhaseUnwrapped.json 
 
 import os
 import sys
@@ -9,72 +9,37 @@ import globals
 from collections import deque
 
 input_file = (sys.argv[1])
-output_file = (sys.argv[2])
-phaseMaxDifference = 0.2                #max allowed diff between average and a point
-distanceImprovementFactor = 0.9
-N_lastPoints = 16
+phaseCorrectionFile = (sys.argv[2])
+output_file = (sys.argv[3])
 
-
-def unwrapPhase(segmentsJSON, phaseKey):
-    global phaseMaxDifference, distanceImprovementFactor, N_lastPoints
-    segments = segmentsJSON["segments"]
-    processed = 0
-    show = False
-    showed = 0
-    maxShowed = 4
-    for segment in segments:
-        samples = segment["samples"]
-        aroundPoints = deque(maxlen=N_lastPoints)
-        if len(samples) == 0:
-            continue
-        averageSamplePhase = samples[0][phaseKey]
-        aroundPoints.append(averageSamplePhase)
-        sampleIndex = -1
-        
-        for sample in samples:
-            sampleIndex+=1
-            phase = sample[phaseKey]
-            newphase = phase
-            aroundPoints = globals.getAroundPoints(sampleIndex, N_lastPoints, samples, phaseKey)
-            averageSamplePhase = globals.pointsAverage(aroundPoints)
-            
-            distance = abs(averageSamplePhase-phase)
-            if distance < phaseMaxDifference:
-                aroundPoints.append(phase)
-                continue
-            
-            increment = round(distance)                                             #only increment/decrement an integer number of times, to keep phase information
-            increasedPhaseDistance = abs(averageSamplePhase-(phase + (increment)))
-            decreasedPhaseDistance = abs(averageSamplePhase-(phase - (increment)))
-            if increasedPhaseDistance < distance*distanceImprovementFactor: newphase = phase+increment          #if we can make it closer to average, we do
-            elif decreasedPhaseDistance < distance*distanceImprovementFactor: newphase = phase-increment
-            sample[phaseKey] = newphase
-            processed+=1
-            aroundPoints.append(newphase)
-            
-            timestamp = sample["timestamp"]
-            if phaseKey == "mobilePhase" and timestamp == "xxxxx1760020163.209353":
-                show = True
-            if show and showed < maxShowed:
-                showed+=1
-                print("averageSamplePhase: ", averageSamplePhase)
-                print("new phase:", newphase)
-                print("-----   timestamp:", timestamp)
-                print("phase:", phase)
-                print("distance:", distance)
-                print("increasedPhaseDistance:", increasedPhaseDistance)
-                print("decreasedPhaseDistance:", decreasedPhaseDistance) 
-                    
-    
-    print("processed: ", processed)
 
 
 with open(input_file, 'r', encoding='utf-8') as f:
-    segmentsJSON = json.load(f)
-unwrapPhase(segmentsJSON, "mobilePhase")
-unwrapPhase(segmentsJSON, "fixedPhase")
+    dataJSON = json.load(f)
+    
+with open(phaseCorrectionFile, 'r', encoding='utf-8') as f:
+    phaseCorrectionJSON = json.load(f)
 
+results = phaseCorrectionJSON
+original = dataJSON[0]["data"]
+
+for key in results:
+    previousPhase = 0
+    data = original[key]
+    begin = 0
+    for result in results[key]:
+        section = result["section"]
+        phaseIncrement = result["localPhaseCorrection"]
+        previousPhase += phaseIncrement
+        end = begin + len(section)
+        for i in range(begin, end):
+            data[i] += previousPhase
+        begin = end
+
+
+
+outputJSON = dataJSON
 
 with open(output_file, 'w', encoding='utf-8') as f:
-    json.dump(segmentsJSON, f, ensure_ascii=False, indent=4)
+    json.dump(outputJSON, f, ensure_ascii=False, indent=4)
 
