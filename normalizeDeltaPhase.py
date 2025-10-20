46d4a424b5d8d41a4a59c355a5439d0960c18f96
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-20 16:53:19 -0500

    .

--- normalizeDeltaPhase.py
+++ normalizeDeltaPhase.py
@@ -12,9 +12,48 @@ import matplotlib.pyplot as plt
 
 input_file = (sys.argv[1])
 output_file = (sys.argv[2])
-standardValue = float(sys.argv[3])
-offsetValue = int(sys.argv[4])
+# standardValue = float(sys.argv[3])
+# offsetValue = int(sys.argv[4])
 phaseMaxDifference = 0.1
+samples = 3000
+
+def processData(hz, data, divider, offset):
+    global samples
+    
+    print("divider: ", divider)
+    print("offset: ", offset)
+    size = samples
+    #size = len(hz)
+    hzModified = np.zeros(size)
+    processed = 0
+    for i in range(size):
+        phase = data[i]
+        hzModified[i] = -hz[i]/divider + offset
+        referenceValue = hzModified[i]
+        distance = abs(referenceValue-phase)                               #actual distance
+        # if distance < phaseMaxDifference:        #assuming the signal difference allowed be lower
+            # continue
+        newphase = phase
+        increment = round(distance)                                             #only increment/decrement an integer number of times, to keep phase information
+        increasedPhaseDistance = abs(referenceValue-(phase + (increment)))
+        decreasedPhaseDistance = abs(referenceValue-(phase - (increment)))
+        if increasedPhaseDistance < distance:
+            newphase = phase+increment          #if we can make it closer to average, we do
+            processed+=1
+        elif decreasedPhaseDistance < distance:
+            newphase = phase-increment
+            processed+=1
+        data[i] = newphase
+
+    xdata = range(size)
+    ydata = data[:size]
+    ydata2 = hzModified[:size]
+    
+    plt.plot(xdata, ydata, '.', label='ds')
+    plt.plot(xdata, ydata2, '.', label='ds')
+    plt.show()
+
+
 
 with open(input_file, 'r', encoding='utf-8') as f:
     dataJSON = json.load(f)
@@ -22,35 +61,17 @@ with open(input_file, 'r', encoding='utf-8') as f:
 hz = dataJSON[0]["data"]["hz"]
 data = dataJSON[0]["data"]["deltaPhase"]          #first element is the original (unmodified) data
 
-size = len(hz)
-hzModified = np.zeros(size)
-for i in range(size):
-    hzModified[i] = -hz[i]/1.5 + 2.75
-
-processed = 0
-pointIndex = -1
-for phase in data:
-    pointIndex +=1
-    referenceValue = hzModified[pointIndex]
-    distance = abs(referenceValue-phase)                               #actual distance
-    # if distance < phaseMaxDifference:        #assuming the signal difference allowed be lower
-        # continue
-    newphase = phase
-    increment = round(distance)                                             #only increment/decrement an integer number of times, to keep phase information
-    increasedPhaseDistance = abs(referenceValue-(phase + (increment)))
-    decreasedPhaseDistance = abs(referenceValue-(phase - (increment)))
-    if increasedPhaseDistance < distance:
-        newphase = phase+increment          #if we can make it closer to average, we do
-        processed+=1
-    elif decreasedPhaseDistance < distance:
-        newphase = phase-increment
-        processed+=1
-    data[pointIndex] = newphase
 
 
-dataJSON[0]["data"]["hzModified"] = hzModified.tolist()
+for i in range(0, 5):
+    for j in range(0, 10):
+        divider = 3 + i/25
+        offset = 1.7 + j/5
+        processData(hz, data, divider, offset)
+        print()
 
-with open(output_file, 'w', encoding='utf-8') as f:
-    json.dump(dataJSON, f, ensure_ascii=False, indent=4)
+# dataJSON[0]["data"]["hzModified"] = hzModified.tolist()
+# with open(output_file, 'w', encoding='utf-8') as f:
+    # json.dump(dataJSON, f, ensure_ascii=False, indent=4)
 
-print("processed: ", processed)
+#print("processed: ", processed)
