d86023c7258394f35294088b968ce4fe7bc07073
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-23 09:45:18 -0500

    .

--- LinearRegressionTest.py
+++ LinearRegressionTest.py
@@ -2,9 +2,20 @@
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+X = np.array([1,2,3])
+print(X)
+X = X.reshape(-1, 1)
+print(X)
+
 # y = 1 * x_0 + 2 * x_1 + 3
-y = np.dot(X, np.array([3.5, 9.04])) - 1.5
+size = len(X)
+y = np.zeros(size)
+for i in range(size):
+    y[i] = (X[i]*(-7.3) - 0.77)
+
+print(y)    
+y = y.reshape(-1, 1)
+print(y)
 reg = LinearRegression().fit(X, y)
 print("reg.score(X, y):", reg.score(X, y))
 print("reg.coef_:", reg.coef_)
