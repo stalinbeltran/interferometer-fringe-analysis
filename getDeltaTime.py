664d0400c958708f423e194ac376c17260b1f60b
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-25 10:25:52 -0500

    get delta time

--- getDeltaTime.py
+++ getDeltaTime.py
@@ -1,11 +1,8 @@
-#python3 getSamples.py ./60hzSegments.json ./60hzSegmentSamples.json
-#python3 getSamples.py ./40hzSegments.json ./40hzSegmentSamples.json
 
 import os
 import sys
 import json
 
-DISTANCE_FACTOR = 0.5
 input_file = (sys.argv[1])
 output_file = (sys.argv[2])
 
@@ -19,11 +16,13 @@ deltatime = []
 
 for segment in segments:
     files = segment["files"]
+    previousTimestamp = files[0]["timestamp"]
     for file in files:
         timestamp = file["timestamp"]
         timestamps.append(float(timestamp))
         timestampDiff =  float(timestamp) - float(previousTimestamp)
         deltatime.append(timestampDiff)
+        previousTimestamp = timestamp
 
 
 newData = [
@@ -36,6 +35,6 @@ newData = [
         }
     ]
     
-with open("D:\\Stalin\\FotosFranjasProyecto\\results\\timestamps.json", 'w', encoding='utf-8') as f:
+with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(newData, f, ensure_ascii=False, indent=4)
 
