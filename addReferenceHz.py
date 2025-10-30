95c0df2e03b569ea5ce442f0bd433b6f309c1603
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-30 12:07:05 -0500

    add hz reference

---
+++ addReferenceHz.py
@@ -0,0 +1,31 @@
+#python3 
+
+import os
+import sys
+import json
+import cv2
+import numpy as np
+import globals
+from collections import deque
+
+input_file = (sys.argv[1])
+output_file = (sys.argv[2])
+
+
+
+
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    
+hz = data[0]["data"]["hz"]
+hzReference = [-1*(x - 6.87)/6 for x in hz]
+data[0]["data"]["hzReference"] = hzReference
+
+print("end")
+
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+print("ended")
